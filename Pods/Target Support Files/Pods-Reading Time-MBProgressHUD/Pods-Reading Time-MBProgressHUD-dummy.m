9c61b44d8e20af1868294e794e6e8c0a354297fa
Nathan Pahucki <npahucki@gmail.com>
2015-07-21 13:51:05 -0300

    Remove files that shouldn't be in source control

--- Pods/Target Support Files/Pods-Reading Time-MBProgressHUD/Pods-Reading Time-MBProgressHUD-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Reading_Time_MBProgressHUD : NSObject
-@end
-@implementation PodsDummy_Pods_Reading_Time_MBProgressHUD
-@end
--- Pods/Target Support Files/Pods-Reading Time/Pods-Reading Time-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Reading_Time : NSObject
-@end
-@implementation PodsDummy_Pods_Reading_Time
-@end
